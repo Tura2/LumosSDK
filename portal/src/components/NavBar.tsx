5c2eb12c4c924dd4b19af92e9f2fc8dbc590730b
Tura2 <offir.tura@gmail.com>
2026-06-18 14:44:47 +0300

    chore(portal): SVG favicon + logo; drop 4.9MB PNG

--- portal/src/components/NavBar.tsx
+++ portal/src/components/NavBar.tsx
@@ -64,7 +64,7 @@ export default function NavBar() {
       {/* Logo */}
       <div style={{ display: 'flex', alignItems: 'center', gap: 10, marginBottom: 36, padding: '0 8px' }}>
         <img
-          src="/lumos-icon.png"
+          src="/lumos-mark.svg"
           width={36} height={36}
           alt="Lumos"
           style={{ borderRadius: 10, objectFit: 'cover', flexShrink: 0 }}
